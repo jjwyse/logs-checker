fe9034756665371d06b7c87ac50a1e23e84ba4a9
jjwyse <josh@cloud-elements.com>
2014-01-11 16:37:21 -0700

    Adding config slurper to configure the files and exceptions you want to watch for

--- src/logs-checker.groovy
+++ src/logs-checker.groovy
@@ -1,4 +1,12 @@
-def reader = new TailReader()
+// load system variables
+def env = System.getenv()
 
+// load config
+def config = new ConfigSlurper("files").parse(new File(env['HOME'] + '/.log-checker/config.groovy').toURI().toURL())
+
+print config
+
+// for each file in the config start up a tail thread
+def reader = new TailReader()
 reader.tail(new File('/tmp/foo.log'), { "terminal-notifier -title log-checker -subtitle foo.log -message $it".execute() })
 reader.tail(new File('/tmp/bar.log'), { "terminal-notifier -title log-checker -subtitle bar.log -message $it".execute() })
