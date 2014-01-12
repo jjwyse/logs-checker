f5dc74a0e92d50912ea61d89b2e195eaeb512ddf
jjwyse <josh@cloud-elements.com>
2014-01-12 09:19:04 -0700

    Removing TailReader class and just making it part of the script

--- src/logs-checker.groovy
+++ src/logs-checker.groovy
@@ -11,47 +11,42 @@ config.config.files.each() { file ->
     String exceptions = file.exceptions
     String command = "terminal-notifier -title log-checker -subtitle " + file.location + " -message Exception Found " +
             "-sound default"
+
+    File logFile = new File(file.location)
+    String [] blacklist = exceptions.split('\\|')
     def closure = {
         command.execute()
     }
-    new TailReader().tail(new File(file.location), exceptions.split("\\|"), closure)
-}
+    def runnable = {
+        def reader
 
-class TailReader
-{
-    public void tail(File file, String[] blacklist, def closure)
-    {
-        def runnable = {
-            def reader
+        try
+        {
+            reader = logFile.newReader()
+            reader.skip(logFile.length())
 
-            try
+            def line
+
+            while (true)
             {
-                reader = file.newReader()
-                reader.skip(file.length())
-
-                def line
-
-                while (true)
+                line = reader.readLine()
+                for (String word : blacklist)
                 {
-                    line = reader.readLine()
-                    for (String word : blacklist)
+                    if (line?.contains(word))
                     {
-                        if (line?.contains(word))
-                        {
-                            closure.call(line)
-                        }
+                        closure.call(line)
                     }
-                    Thread.currentThread().sleep(2000)
                 }
-
+                Thread.currentThread().sleep(2000)
             }
-            finally
-            {
-                reader?.close()
-            }
-        } as Runnable
 
-        def t = new Thread(runnable)
-        t.start()
-    }
+        }
+        finally
+        {
+            reader?.close()
+        }
+    } as Runnable
+
+    def t = new Thread(runnable)
+    t.start()
 }
