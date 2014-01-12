c43d02622f10e4da161f3957706c313c2345bf26
jjwyse <josh@cloud-elements.com>
2014-01-12 09:16:33 -0700

    Making TailReader an inner class so we're not dependent upon any groovy classe

--- src/logs-checker.groovy
+++ src/logs-checker.groovy
@@ -7,12 +7,51 @@ def env = System.getenv()
 def config = new ConfigSlurper().parse(new File(env['HOME'] + '/.logs-checker/config.groovy').text)
 
 // for each file in the config start up a tail thread
-def reader = new TailReader()
 config.config.files.each() { file ->
     String exceptions = file.exceptions
-    String command = "terminal-notifier -title log-checker -subtitle " + file.location + " -message Exception Found -sound default"
+    String command = "terminal-notifier -title log-checker -subtitle " + file.location + " -message Exception Found " +
+            "-sound default"
     def closure = {
         command.execute()
     }
-    reader.tail(new File(file.location), exceptions.split("\\|"), closure)
+    new TailReader().tail(new File(file.location), exceptions.split("\\|"), closure)
+}
+
+class TailReader
+{
+    public void tail(File file, String[] blacklist, def closure)
+    {
+        def runnable = {
+            def reader
+
+            try
+            {
+                reader = file.newReader()
+                reader.skip(file.length())
+
+                def line
+
+                while (true)
+                {
+                    line = reader.readLine()
+                    for (String word : blacklist)
+                    {
+                        if (line?.contains(word))
+                        {
+                            closure.call(line)
+                        }
+                    }
+                    Thread.currentThread().sleep(2000)
+                }
+
+            }
+            finally
+            {
+                reader?.close()
+            }
+        } as Runnable
+
+        def t = new Thread(runnable)
+        t.start()
+    }
 }
