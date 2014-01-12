47b6f360ff9ffc87f251307c7b7617a65a241fab
jjwyse <josh@cloud-elements.com>
2014-01-11 23:29:16 -0700

    Working on README

--- README.md
+++ README.md
@@ -5,4 +5,21 @@ strings appears, it will send a desktop notification
 
 setup
 ==============
-* If you don't have 'terminal-notifier' installed, do so <a href="https://github.com/alloy/terminal-notifier" target="_blank">here</a>
+* If you don't have the command-line tool 'terminal-notifier' installed, do so <a href="https://github.com/alloy/terminal-notifier" target="_blank">here</a>
+* Create the logs-checker config file here $HOME/.logs-checker/config.groovy - below is an example
+
+```
+config {
+  files = [
+    [
+      'location': '/tmp/bar.log',
+      'exceptions': 'bar|foo'
+    ],
+    [
+      'location': '/tmp/foo.log',
+      'exceptions': 'bar|foo|exception|NullPointerException'
+    ]
+  ]
+}
+
+```
--- src/logs-checker.groovy
+++ src/logs-checker.groovy
@@ -4,13 +4,13 @@
 def env = System.getenv()
 
 // load config from $HOME/.logs-checker directory
-def config = new ConfigSlurper().parse(new File(env['HOME'] + '/.log-checker/config.groovy').text)
+def config = new ConfigSlurper().parse(new File(env['HOME'] + '/.logs-checker/config.groovy').text)
 
 // for each file in the config start up a tail thread
 def reader = new TailReader()
 config.config.files.each() { file ->
     String exceptions = file.exceptions
-    String command = "terminal-notifier -title log-checker -subtitle " + file.location + " -message Exception Found"
+    String command = "terminal-notifier -title log-checker -subtitle " + file.location + " -message Exception Found -sound default"
     def closure = {
         command.execute()
     }
